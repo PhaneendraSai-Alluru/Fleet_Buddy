dbef8dc8d40a95a117a438bd358265f21e3bff16
Sai <alluruphaneendrasai@gmail.com>
2026-04-14 00:14:43 +0530

    First Baseline Commit and Push for Fleet Buddy Project. First Page completed with designs and dummy data.

---
+++ lib/main.dart
@@ -0,0 +1,52 @@
+import 'package:fleet_buddy/input_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Fleet Buddy',
+      theme: ThemeData.light(),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Fleet Buddy'),
+           ),
+          drawer: Drawer(
+              child: ListView(
+                // Important: Remove any padding from the ListView.
+                padding: EdgeInsets.zero,
+                children: [
+                  const DrawerHeader(
+                    decoration: BoxDecoration(color: Colors.blue),
+                    child: Text('Drawer Header'),
+                  ),
+                  ListTile(
+                    title: const Text('Item 1'),
+                    onTap: () {
+                      // Update the state of the app.
+                      // ...
+                    },
+                  ),
+                  ListTile(
+                    title: const Text('Item 2'),
+                    onTap: () {
+                      // Update the state of the app.
+                      // ...
+                    },
+                  ),
+                ],
+              ),
+          ),
+          body: SafeArea(child: screen1()
+          ),
+          ),
+          );
+  }
+}
+
